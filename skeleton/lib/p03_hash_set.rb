9ed7455e7488b78a662bd3b06e523e27a7e72cb5
RobandDoug <RobandDoug@email>
2015-10-09 14:16:04 -0400

    Implement hash set class

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -1,5 +1,6 @@
 require_relative 'p02_hashing'
 
+
 class HashSet
   attr_reader :count
 
@@ -9,18 +10,29 @@ class HashSet
   end
 
   def insert(key)
+    inspect
+    self[key] << key
+    @count +=1
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    self[key].delete(key)
+    @count -= 1
   end
 
   private
 
+  def inspect
+    resize! if @count >= num_buckets
+  end
+
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +40,14 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { [] }
+    @count = 0
+
+    old_store.flatten.each do |el|
+      insert(el)
+    end
+
+    nil
   end
 end
